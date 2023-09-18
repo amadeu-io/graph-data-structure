de748602746b5827dd74ddbd41c0f1a7d60b14ee
Amadeu Serras <118369464+nightrunner4@users.noreply.github.com>
2023-09-18 13:12:01 -0500

    Improve comments

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 class Graph {
   constructor() {
-    // every graph that's created using the Graph class will be an object with a key  of nodes
+    // every graph that's created using the Graph class will be an object with a key of nodes
     // and a value that's a map containing the graph
     this.nodes = new Map();
   }
 
   addNode(node) {
+    // has(key): checks if a key exists in the map and returns a Boolean
+    // so the if statement prevents the addition of a duplicate node with the same key
     if (!this.nodes.has(node)) {
       // set(key, value): adds or updates a key-value pair in the map
       this.nodes.set(node, []);
@@ -14,6 +16,7 @@ class Graph {
 
   // directed edges (they have a direction)
   addEdge(node1, node2) {
+    // makes sure node1 & node2 exist
     if (this.nodes.has(node1) && this.nodes.has(node2)) {
       // get retreives the value of the key-value pair
       // in this case, it's a list containing the linked nodes
